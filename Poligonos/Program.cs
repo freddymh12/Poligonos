91c43713d783cf18b456eff70ca5b1bda336c8c6
FreddyMH <FreddyMH@DESKTOP-ICJSG9Q>
2024-02-13 21:55:45 -0600

    Agregar archivos de proyecto.

---
+++ Poligonos/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poligonos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Desea calcular area o perimetro");
+            Console.WriteLine("1. Perimetro");
+            Console.WriteLine("2. Area");
+            int op = int.Parse(Console.ReadLine());
+            switch (op)
+            {
+                case 1:
+                Console.WriteLine("perimetro");
+                Console.Write("Dame el numero de lados del polígono: ");
+                int lados = int.Parse(Console.ReadLine());
+                Console.Write("Dame la longitud de un lado del polígono: ");
+                double longitud = double.Parse(Console.ReadLine());
+                double perimetro = Calculo(lados, longitud);
+                Console.WriteLine($"El perimetro del polígono de {lados} y la longitud de {longitud} es de: {perimetro}");
+                Console.ReadKey();
+                break;
+
+                case 2:
+                    Console.WriteLine("area");
+                    Console.Write("Dame el perimetro del poligono: ");
+                    double perimetro2 = double.Parse(Console.ReadLine());
+                    Console.Write("Dame la longitud del apotema: ");
+                    double apotema = double.Parse(Console.ReadLine());
+                    double area = Calculo2(perimetro2, apotema);
+                    Console.WriteLine($"El area del polígono es de: {area}");
+                    Console.ReadKey();
+                    break;
+
+                default:
+                    Console.WriteLine(" Ingrese una opcion valida");
+                break;
+
+            }
+            
+
+        }
+        static double Calculo(int lados, double longitud)
+        {
+            double perimetro = lados * longitud;
+            return perimetro;
+        }
+        static double Calculo2(double perimetro2, double apotema)
+        {
+            double area = (perimetro2 * apotema) / 2;
+            return area;
+        }
+        
+        }
+    }
+
---
+++ Poligonos/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Poligonos")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Poligonos")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("7efd5290-53c0-4710-a603-f9a7d64bda99")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
